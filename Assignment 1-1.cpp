c27a9941a4b77b568d9252f9a9efecf5d3b2e605
Shinycolours <silvarobert210@gmail.com>
2021-02-10 05:17:13 +0000

    First commit

---
+++ Assignment 1-1.cpp
@@ -0,0 +1,25 @@
+#include <iosstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  const double rateA = 15.00;
+  const double rateB = 12.00;
+  const double rateC = 9.00;
+
+  int seatA, seatB, seatC;
+  cout << "How many tickets from class A were sold?" << endl;
+  cin seatA;
+  cout << "How many tickets from class B were sold?" << endl;
+  cin seatB;
+  cout << "How many tickets from class C were sold?" << endl;
+  cin seatC;
+
+  double cashseatA = rateA * seatA;
+  double cashseatB = rateB * seatB;
+  double cashseatC = rateC * seatC;
+  double total = cashseatA + cashseatB + cashseatC;
+  
+  cout << "You made $" << fixed <<showpoint << setprecision(2) << total << "for all seats." << endl;
+}
