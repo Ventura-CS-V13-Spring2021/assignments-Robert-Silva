1f0ce5fb3745218d100b03f3433195d98dbadaf1
Shinycolours <silvarobert210@gmail.com>
2021-02-23 05:33:42 +0000

    First commit

---
+++ Assignment 2-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int number1, number2, number3;
+     cout << "Enter three integer values : " ;
+     cin >> number1 >> number2 >> number3 ;
+     
+     if (number1 == number2)
+      cout<< "There are two duplicate numbers" << endl;
+    
+    else if(number1 == number3)
+      cout<< "There are two duplicate numbers" << endl;
+    
+    else if(number2 == number3)
+      cout << "There are two duplicate numbers" << endl;
+
+    else if((number1 == number2) && (number2 == number3))
+      cout << "All numbers are the same" << endl;
+
+    else
+      cout<< "All numbers are distinct" << endl;
+     return 0;
+}
