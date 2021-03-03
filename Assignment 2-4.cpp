06ce67affccc353b4dd2923d99142780ccedf631
Shinycolours <silvarobert210@gmail.com>
2021-03-03 03:40:05 +0000

    first commit

---
+++ Assignment 2-4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char letters;
+  int i = 65;
+
+  while(i<91) 
+  {
+    letters = i;
+
+    if(letters % 5 == 4)
+    {
+      cout << letters << endl;
+    }
+    else 
+    {
+      cout << letters <<
+    }
+  }
+  
+  
+}
